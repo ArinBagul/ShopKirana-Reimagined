4568643ce722fd36adee7fe92634f2f42685fc29
Arin Bagul <prototypearin@gmail.com>
2024-08-22 17:12:17 +0530

    Created responsive navbar and responsive landing page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Advt from "./components/Advt";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
-// import BottomFX from "./components/BottomFX";
+import BottomFX from "./components/BottomFX";
 
 import "./App.css";
 import Error404 from "./components/Error404";
@@ -25,8 +25,8 @@ function App() {
       <Advt />
       <Contact />
       <Footer />
-      {/* <BottomFX /> */}
       <Error404 />
+      <BottomFX />
     </>
   );
 }
